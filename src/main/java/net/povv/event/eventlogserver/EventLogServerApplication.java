ae770131b5d00dfea5f0d5b53723803fc8ba69be
jeffchoi72 <ihello0720@gmail.com>
2021-07-02 08:04:39 +0900

    chore: 프로젝트를 설정한다

---
+++ src/main/java/net/povv/event/eventlogserver/EventLogServerApplication.java
@@ -0,0 +1,13 @@
+package net.povv.event.eventlogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventLogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventLogServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/povv/event/eventlogserver/EventLogServerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.povv.event.eventlogserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventLogServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
